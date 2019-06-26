6cec4f149ca25a8402e99a09b74afdcb2a7dda74
fgkcv25 <51954835+fgkcv25@users.noreply.github.com>
2019-06-26 11:49:40 -0300

    melhoras w/e

--- Tarefa 6/Fibonacci.py
+++ Tarefa 6/Fibonacci.py
@@ -5,8 +5,13 @@ Created on Thu Jun 20 21:18:31 2019
 @author: CLIENTE
 """
 
+
 def fibonacci(n):
     if n == 1 or n == 2:
         return 1
     else:
         return fibonacci(n-2) + fibonacci(n-1)
+
+n = 5
+
+print(fibonacci(n))
---
+++ Tarefa 6/chama a soma.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 26 11:19:08 2019
+
+@author: 05873472955
+"""
+
+from soma import soma
+
+#soma 1 com 4 e mostra o resultado!
+print(soma(1,4))
