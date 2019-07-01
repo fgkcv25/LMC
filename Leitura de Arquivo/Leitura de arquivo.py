5cae5a195164bd66cd01fafc46443ed80c721f41
fgkcv25 <51954835+fgkcv25@users.noreply.github.com>
2019-07-01 14:57:58 -0300

    agora ele bota as matrizes conforme vai indo
    problema no C = str(C), provavelmente contornável

--- Leitura de Arquivo/Leitura de arquivo.py
+++ Leitura de Arquivo/Leitura de arquivo.py
@@ -8,7 +8,7 @@ Created on Tue May 21 07:51:07 2019
 
 import numpy as np
 
-with open('C:\\Users\\05873472955\\Downloads\\LMC\\LMC\\Leitura de Arquivo\\A.txt') as arquivo:
+with open('C:\\Users\\05873472955\\Desktop\\LEitura de Arquivo\\A.txt') as arquivo:
     dados = arquivo.readlines()
     
 dados.remove(dados[1001])
@@ -21,7 +21,7 @@ for linha in dados:
 A = np.asarray(A, dtype=np.float64)
 
 
-with open('C:\\Users\\05873472955\\Downloads\\LMC\\LMC\\Leitura de Arquivo\\B.txt') as arquivoB:
+with open('C:\\Users\\05873472955\\Desktop\\LEitura de Arquivo\\B.txt') as arquivoB:
     dadosB = arquivoB.readlines()
     
 dadosB.remove(dadosB[1507])
@@ -43,26 +43,25 @@ while i < len(B):
 B = np.asarray(B, dtype=np.float64)
     
     
-arquivoC = open('C:\\Users\\05873472955\\Downloads\\LMC\\LMC\\Leitura de Arquivo\\C.txt','w+')
+arquivoC = open('C:\\Users\\05873472955\\Desktop\\LEitura de Arquivo\\C.txt','w+')
 (m,n) = A.shape
 (p,q) = B.shape
-    
-C = np.zeros((p,n*q))
+
+
+listadematrizes = [1*B,2*B,3*B,4*B,5*B,6*B,7*B,8*B,9*B,10*B]
     
 for Ai in range(m):
-    for Bi in range(p):
-        for Aj in range(n):
-            for Bj in range(q):
-                C[Bi,Aj*q+Bj] = A[Ai,Aj]*B[Bi,Bj]
-    s = list(C)
-    for i in range(len(s)):
-        s[i] = list(s[i])
-    s = str(s)
-    s = s.rstrip(']').replace(']','\n').replace('.0','').replace(',','').replace('[','')
-    arquivoC.write(s)
+    C = []
+    for Aj in range(n):
+        C.append(listadematrizes[int(A[Ai][Aj])-1])
+    for i in range(len(C)):
+        C[i] = list(C[i])
+    C = str(C)
+    C = C.rstrip(']').replace(']','\n').replace('.0','').replace(',','').replace('[','')
+    arquivoC.write(C)
     
 arquivoC.close()
 
 
     
-
+ 
